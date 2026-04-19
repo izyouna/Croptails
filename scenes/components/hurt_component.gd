94caea186cd49056e58c9f5599b9234893148025
izyouna <chakrit4100@gmail.com>
2026-04-19 23:46:02 +0700

    Make 2d Tree Shake with Vertex Shader Effects

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -8,5 +8,5 @@ signal hurt
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
 	
-	if tool == hit_component.current_tool:
+	if tool != hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
--- scenes/components/interactable_component.gd
+++ scenes/components/interactable_component.gd
@@ -8,6 +8,5 @@ signal interactable_deactivated
 func _on_body_entered(body: Node2D) -> void:
 	interactable_activated.emit()
 
-
 func _on_body_exited(body: Node2D) -> void:
 	interactable_deactivated.emit()
